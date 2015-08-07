c4e9ad6944755086acab7bee669f2aaee1bcc1ec
rms <nyubis@gmail.com>
2015-08-07 18:03:09 +0200

    Debug output for admin

--- modules/admin/admin.go
+++ modules/admin/admin.go
@@ -1,12 +1,14 @@
 package admin
 
 import (
+	"fmt"
 	"github.com/nyubis/mibot/core"
 	"github.com/nyubis/mibot/utils"
 )
 
 func CheckAdmin(nick string) bool {
 	if utils.Contains(core.Config.Admins.Admins, nick) {
+		fmt.Println(nick, "verified as admin")
 		return IsAuthenticated(nick)
 	}
 	return false
