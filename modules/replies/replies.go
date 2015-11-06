f1052e850036de8d89503506a2a7992502e4fab1
Roel <nyubis@gmail.com>
2015-11-06 23:44:24 +0100

    Update replies to work with new config

--- modules/replies/replies.go
+++ modules/replies/replies.go
@@ -8,7 +8,8 @@ import (
 )
 
 var bot *core.Bot
-var replies map[string]string
+var globalreplies map[string]string
+var channelreplies map[string]map[string]string
 
 func Init(ircbot *core.Bot) {
 	bot = ircbot
@@ -16,20 +17,30 @@ func Init(ircbot *core.Bot) {
 }
 
 func LoadCfg() {
-	replies = make(map[string]string)
-	for _, line  := range core.Config.Replies.Replies {
-		if !strings.Contains(line, "\t") {
-			fmt.Println("Warning: No tab found in line %s, reply was not added", line)
-			continue
+	channelreplies= make(map[string]map[string]string)
+	for k, v := range core.Config.Channelsettings {
+		if k == "global" {
+			globalreplies = v.Replies
+		} else {
+			channelreplies[k] = v.Replies
 		}
-		split := strings.Split(line, "\t")
-		replies[split[0]] = split[1]
 	}
 }
 
 
 func Handle(msg ircmessage.Message) {
-	reply, has := replies[msg.Content]
+	// Check whether we have a set of replies specific to this channel
+	replyset, haschannel := channelreplies[msg.Channel]
+	if haschannel {
+		// Check whether the trigger is in the set
+		reply, has := replyset[msg.Content]
+		if has {
+			bot.SendMessage(msg.Channel, reply)
+			return
+		}
+	}
+	// Check the global reply set
+	reply, has := globalreplies[msg.Content]
 	if has {
 		bot.SendMessage(msg.Channel, reply)
 	}
